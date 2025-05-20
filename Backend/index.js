086436081f926ba75906ae45103fd80b0293a315
Saikat Bahadur <saikatbahadur1234@gmail.com>
2025-05-20 17:08:34 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,12 +6,18 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors({
-        origin: process.env.FRONTEND_URL || "http://localhost:5173",
-        credentials: true
-    }
-));
+// ✅ Allow your frontend URL
+const allowedOrigins = ['https://news-website-frontend-dun.vercel.app'];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 const PORT = process.env.PORT || 3000;
 
 app.get("/api/news", async (req, res) => {
